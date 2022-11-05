4c826edfa359a1451fb43ec68ba4527b13feaee7
Fauzi Abdul Rohim <fauziboyranger@gmail.com>
2022-11-06 03:47:18 +0700

    Update FooterRequest.php

--- app/Http/Requests/FooterRequest.php
+++ app/Http/Requests/FooterRequest.php
@@ -34,7 +34,8 @@ class FooterRequest extends FormRequest
                 'whatsapp'      => ['required','numeric'],
                 'telp'          => ['required','numeric'],
                 'email'         => ['required','email'],
-                'desc'          => [new MaxCharacters(200), 'required'],
+                // 'desc'          => [new MaxCharacters(200), 'required'],
+                'desc'          => ['required'],
             ];
         }
     }
